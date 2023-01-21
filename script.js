291bb7459633e33d4dd73e729a41295903f0c65b
imcgz <igorcts1@gmail.com>
2023-01-20 21:35:21 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+const button = document.querySelector(" header button");
+const nlwSetup = new NLWSetup(form);
+
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+  if (dayExists) {
+    alert("Dia ja encluso! 🔴");
+    return;
+  }
+  alert("Dia adicionado com sucesso! 🟢");
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
